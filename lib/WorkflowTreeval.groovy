f055cbb2cd1ecdab20c02c36a0d983817af4a880
DLBPointon <damonlbp@hotmail.co.uk>
2022-09-13 14:05:05 +0100

    Updates and changes to enable running

--- lib/WorkflowTreeval.groovy
+++ lib/WorkflowTreeval.groovy
@@ -9,24 +9,23 @@ class WorkflowTreeval {
     //
     public static void initialise(params, log) {
         genomeExistsError(params, log)
-
-        if (!params.fasta) {
-            log.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
-            System.exit(1)
-        }
+        //if (!params.fasta) {
+        //    log.error "Genome fasta file not specified with e.g. '--fasta genome.fa' or via a detectable config file."
+        //    System.exit(1)
+        //}
     }
 
     //
     // Exit pipeline if incorrect --genome key provided
     //
     private static void genomeExistsError(params, log) {
-        if (params.genomes && params.genome && !params.genomes.containsKey(params.genome)) {
-            log.error "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
-                "  Genome '${params.genome}' not found in any config files provided to the pipeline.\n" +
-                "  Currently, the available genome keys are:\n" +
-                "  ${params.genomes.keySet().join(", ")}\n" +
-                "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-            System.exit(1)
-        }
+        //if (params.genomes && params.genome && !params.genomes.containsKey(params.genome)) {
+        //    log.error "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" +
+        //        "  Genome '${params.genome}' not found in any config files provided to the pipeline.\n" +
+        //        "  Currently, the available genome keys are:\n" +
+        //        "  ${params.genomes.keySet().join(", ")}\n" +
+        //        "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+        //    System.exit(1)
+        //}
     }
 }
