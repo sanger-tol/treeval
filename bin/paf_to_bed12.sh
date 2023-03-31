574f8e4f575211d81f186a842818ec321e795490
DLBPointon <damonlbp@hotmail.co.uk>
2023-03-31 13:02:43 +0100

    Scripts to complement new local modules

---
+++ bin/paf_to_bed12.sh
@@ -0,0 +1,1 @@
+cat $1 | awk 'BEGIN{FS="\t";}{a[$1]++;if(a[$1]==2)print v[$1] ORS $0;if(a[$1]>2)print;v[$1]=$0;}' | awk '$(NF+1) = ($10/$11)*100' | awk '$(NF+1) = ($10/$2)*100' | awk -vOFS='\t' '{print $6,$8,$9,$1,$2,$10,$(NF-1),$NF}' > punchlist.bed
