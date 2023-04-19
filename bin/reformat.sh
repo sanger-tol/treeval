62c45d507be6b46a14fd5782165c49aa3621a881
DLBPointon <damonlbp@hotmail.co.uk>
2023-04-19 10:39:43 +0100

    Updated  bash script to include version, author and explainer

--- bin/reformat.sh
+++ bin/reformat.sh
@@ -1,1 +1,18 @@
-awk '{print $0"\t"sqrt(($3-$2)*($3-$2))}'|sed 's/\./0/g'
+#!/bin/bash
+
+# gff_to_bed.sh
+# -------------------
+# A shell script to convert a
+# gff into bed format by stripping the
+# PAF header and reorganising the data
+# -------------------
+# Author = yy5
+
+version='1.0.0'
+
+if [ $1 == '-v'];
+then
+    echo "$version"
+else
+    awk '{print $0"\t"sqrt(($3-$2)*($3-$2))}'|sed 's/\./0/g'
+fi
