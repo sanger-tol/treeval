c339eae31a3e5bddbaac6ef85a41d3a9cf1f19b7
yumisims <yy5@sanger.ac.uk>
2024-02-01 22:27:25 +0000

    using mulled

--- bin/grep_pg.sh
+++ bin/grep_pg.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+# grep_pg.sh
+# -------------------
+# A shell script to exclude pg lines and label read 1 and read 2 from cram containers
+#
+# -------------------
+# Author = yy5
+
 grep -v "^\@PG" | awk '{if($1 ~ /^\@/) {print($0)} else {if(and($2,64)>0) {print(1$0)} else {print(2$0)}}}'
